1bbc20b7cbcc68922d66ba46a35f92616a9d454b
Mohamed-Mohamed-Hozien <s-mohamed.hozien@zewailcity.edu.eg>
2024-05-14 20:45:13 +0300

    chore: Refactor Helpers.py and Receiver.py
    adding plots

--- FileTransfer.py
+++ FileTransfer.py
@@ -1,9 +1,13 @@
-from socket import (socket, 
-                    AF_INET, 
+from socket import (socket,
+                    AF_INET,
                     SOCK_DGRAM)
 from Helpers import *
 import random
 import sys
+import time
+import matplotlib.pyplot as plt
+from datetime import datetime
+
 
 class FileTransfer:
     def __init__(self, hostname, port, window_size, timeout):
@@ -16,53 +20,102 @@ class FileTransfer:
         self.socket = socket(AF_INET, SOCK_DGRAM)
         self.timeout = timeout
         self.socket.settimeout(self.timeout)
-        
+        self.packet_log = []
+
     def SendFile(self, filepath):
+        start_T = time.time()
         packets = FileToSenderPackets(filepath, self.file_id)
         window_start = 0
         window_end = min(window_start + self.window_size, len(packets) - 1)
-        while window_start < len(packets) - 1:       
+        numOfBytes = 0
+        retransmits = 0
+        while window_start < len(packets) - 1:
             acks = set()
-            
             for i in range(window_start, window_end):
                 if random.random() <= 0.15:
                     continue
                 self.socket.sendto(packets[i], (self.hostname, self.port))
-                
+                numOfBytes += len(packets[i])
+                sended_T = datetime.now()
+                self.packet_log.append((sended_T, i, 'sent'))
+
             timeout = 0
             while timeout < self.timeout * (window_end - window_start) and len(acks) < window_end - window_start:
                 try:
                     ack, _ = self.socket.recvfrom(16)
-                    seqeunce, file_id = LongBytesToNum(ack[:8]), LongBytesToNum(ack[8:])
+                    sequence, file_id = LongBytesToNum(
+                        ack[:8]), LongBytesToNum(ack[8:])
                     if file_id == self.file_id:
-                        acks.add(seqeunce)
+                        acks.add(sequence)
+                        ack_time = datetime.now()
+                        self.packet_log.append((ack_time, sequence, 'ack'))
                 except:
                     pass
                 timeout += self.timeout
-            
+
             if len(acks) == window_end - window_start:
                 window_start = window_end
             else:
                 for i in range(window_start, window_end):
                     if not i in acks:
                         window_start = i
+                        retransmits += 1
+                        self.packet_log.append(
+                            (datetime.now(), i, 'retransmit'))
                         break
             window_end = min(window_start + self.window_size, len(packets) - 1)
-            
+
         ack = None
         while not ack:
             try:
                 self.socket.sendto(packets[-1], (self.hostname, self.port))
+                numOfBytes += len(packets[-1])
                 ack, _ = self.socket.recvfrom(16)
-                seqeunce, file_id = LongBytesToNum(ack[:8]), LongBytesToNum(ack[8:])
+                seqeunce, file_id = LongBytesToNum(
+                    ack[:8]), LongBytesToNum(ack[8:])
             except:
                 pass
-            
+
         self.file_id += 1
-        
+        end_T = time.time()
+        overall_time = end_T - start_T
+        print(f"File sent: {filepath}",
+              f"Start time: {start_T}",
+              f"End time: {end_T}",
+              f"Elapsed time: {overall_time} seconds",
+              f"Number of packets: {len(packets)}",
+              f"Number of bytes: {numOfBytes}",
+              f"Number of retransmits: {retransmits}",
+              "Dn", sep="\n")
+        self.plot_packet_log()
+
+    def plot_packet_log(self):
+        times_sent = [log[0] for log in self.packet_log if log[2] == 'sent']
+        times_ack = [log[0] for log in self.packet_log if log[2] == 'ack']
+        times_retransmit = [log[0]
+                            for log in self.packet_log if log[2] == 'retransmit']
+        seq_sent = [log[1] for log in self.packet_log if log[2] == 'sent']
+        seq_ack = [log[1] for log in self.packet_log if log[2] == 'ack']
+        seq_retransmit = [log[1]
+                          for log in self.packet_log if log[2] == 'retransmit']
+
+        plt.figure(figsize=(10, 6))
+        plt.plot(times_sent, seq_sent, 'bo-', label='Sent Packets')
+        plt.plot(times_ack, seq_ack, 'go-', label='Acknowledged Packets')
+        plt.plot(times_retransmit, seq_retransmit,
+                 'ro-', label='Retransmitted Packets')
+        plt.xlabel('Time')
+        plt.ylabel('Packet Sequence Number')
+        plt.title('Packet Sequence Number vs Time')
+        plt.grid(True)
+        plt.legend()
+        plt.show()
+
+
 if __name__ == "__main__":
     if len(sys.argv) != 4:
-        print("Usage: python FileTransfer.py [file name] [window size] [timeout]")
+        print(
+            "Usage: python FileTransfer.py [file name] [window size] [timeout]")
         sys.exit()
     file = sys.argv[1]
     window_size = int(sys.argv[2])
--- Reciever.py
+++ Reciever.py
@@ -1,14 +1,19 @@
-from socket import (socket, 
-                    AF_INET, 
+from socket import (socket,
+                    AF_INET,
                     SOCK_DGRAM)
 from Helpers import *
 import sys
+import time
+import matplotlib.pyplot as plt
+from datetime import datetime
+
 
 class FileReceiver:
     def __init__(self, port):
         self.port = port
         self.sock = socket(AF_INET, SOCK_DGRAM)
         self.sock.bind(("localhost", port))
+        self.packet_log = []
 
     def send_acknowledgement(self, sequence, file_id, address):
         ack_packet = MakeReceiverPacket(sequence, file_id)
@@ -24,20 +29,50 @@ class FileReceiver:
                 file.write(packet["Data"])
 
     def receive_file(self, save_path):
+        start_T = time.time()
         packets = []
         recieved_seq = set()
+        numOfBytes = 0
         while True:
             packet, address = self.sock.recvfrom(256)
+            recv_T = datetime.now()
             unpacked_packet = Unpack(packet)
-            self.send_acknowledgement(unpacked_packet["Sequence"], unpacked_packet["FileID"], address)
+            self.send_acknowledgement(
+                unpacked_packet["Sequence"], unpacked_packet["FileID"], address)
             if not unpacked_packet["Sequence"] in recieved_seq:
                 packets.append(unpacked_packet)
                 recieved_seq.add(unpacked_packet["Sequence"])
-            
+                self.packet_log.append(
+                    (recv_T, unpacked_packet["Sequence"]))
+                numOfBytes += len(unpacked_packet["Data"])
+
             if unpacked_packet["Trailer"] == 1:
                 break
         ordered_packets = self.order_packets(packets)
         self.write_to_file(ordered_packets, save_path)
+        end_T = time.time()
+        overall_time = end_T - start_T
+        print(f"File received: {save_path}",
+              f"Start time: {start_T}",
+              f"End time: {end_T}",
+              f"Elapsed time: {overall_time} seconds",
+              f"Number of packets: {len(packets)}",
+              f"Number of bytes: {numOfBytes}",
+              "Dn", sep="\n")
+        print()
+        self.plot_packet_log()
+
+    def plot_packet_log(self):
+        times, sequences = zip(*self.packet_log)
+        plt.figure(figsize=(10, 6))
+        plt.plot(times, sequences, 'bo-', label='Received Packets')
+        plt.xlabel('Time')
+        plt.ylabel('Packet Sequence Number')
+        plt.title('Packet Sequence Number vs Time')
+        plt.grid(True)
+        plt.legend()
+        plt.show()
+
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
